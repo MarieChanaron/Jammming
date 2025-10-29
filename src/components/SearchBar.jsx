78c764faabfa8018db2c381a1bf55aab177be57f
Marie Chanaron <chanaronmarie@gmail.com>
2025-10-29 17:19:20 +0100

    Header styled

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,5 +1,12 @@
 function SearchBar() {
-
+    return (
+        <div id="searchbar">
+            <form>
+                <input type="text" />
+                <input type="submit" value="Search" />
+            </form>
+        </div>
+    );
 }
 
 export default SearchBar;
---
+++ src/components/heading.jsx
@@ -0,0 +1,9 @@
+function Heading() {
+    return (
+        <div id="heading">
+            <h1><span>Ja</span><span style={{color: "#8e40e2"}}>mmm</span><span>ing</span></h1>
+        </div>
+    );
+}
+
+export default Heading;
