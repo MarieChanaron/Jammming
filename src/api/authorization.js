f9943890619902f8d8497c3a6ea5990fd3a4cad1
Marie Chanaron <chanaronmarie@gmail.com>
2025-10-30 19:05:48 +0100

    Hide credentials

--- src/api/authorization.js
+++ src/api/authorization.js
@@ -1,16 +1,60 @@
 import credentials from "./credentials";
 const { CLIENT_ID, CLIENT_SECRET } = credentials;
 
-const URL = 'https://accounts.spotify.com/api/token';
+
+const getToken = () => {
+
+  const URL = 'https://accounts.spotify.com/api/token';
+
+  const body = new URLSearchParams({
+    grant_type: 'client_credentials',
+    client_id: `${CLIENT_ID}`,
+    client_secret: `${CLIENT_SECRET}`
+  });
+
+  const headers = {
+    'Content-Type': 'application/x-www-form-urlencoded'
+  }
+
+  fetch(URL, {
+    method: 'POST',
+    headers: headers,
+    body: body.toString()
+  })
+  .then(response => {
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+    return response.json();
+  })
+  .catch(error => {
+      console.error('Error fetching token:', error);
+  });
+
+};
+
+const promise = new Promise((success, error) => getToken());
+promise.then(r=>console.log(r));
+
+// const tokenPromise = new Promise((onSuccess, onError) => {
+//   getToken()
+//     .then(resp => onSuccess(resp))
+//     .catch(message => onError(message))
+// });
+// tokenPromise.then
+
+console.log(await getToken());
 
 // Construct the request body
-const body = new URLSearchParams({
-  grant_type: 'client_credentials',
-  client_id: `${CLIENT_ID}`,
-  client_secret: `${CLIENT_SECRET}`
-});
+// const body = new URLSearchParams({
+//   grant_type: 'client_credentials',
+//   client_id: `${CLIENT_ID}`,
+//   client_secret: `${CLIENT_SECRET}`
+// });
 
-const fetchTokenPromise = fetch(URL, {
+
+
+/*const fetchTokenPromise = fetch(URL, {
   method: 'POST',
   headers: {
     'Content-Type': 'application/x-www-form-urlencoded'
@@ -31,3 +75,4 @@ fetchTokenPromise
     .catch(error => {
         console.error('Error fetching token:', error);
     });
+*/
